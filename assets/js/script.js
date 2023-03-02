90e940a6ada857ad27654eb23d828e50e71b3b94
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-03-02 18:54:02 -0500

    adjusted Css and html placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,31 +9,83 @@
 // THEN I am again presented with current and future conditions for that city
 
 // searchRecent <buttons>
-// forcastToday - <div> for today
-// forcastFive - firstChild :5-day forecast
-// forcastDays - row for the input
+// forecastToday - <div> for today
+// forecastFive - firstChild :5-day forecast
+// forecastDays - row for the input
 var apiKey = "85b2ec11dc0efbf1ecf90462e1fcb01d"
+var weatherSearchHistory = JSON.parse(localStorage.getItem("weatherSearches"));
+if (weatherSearchHistory === null) {
+    weatherSearchHistory = {
+        home: '',
+        list: [],
+    };
+};
+//load local storage weatherSearchHistory { home: "", recent: ["", ""]} / populate home dropdown
 
+function buildWeather(weatherData) {
+    $(".forecastToday").empty();
+
+};
+
+//buildforecast()
+
+//updateHistory() push to recents / save to storage /itterate backwards / clear() and build buttons
+
+
+
+const recentQueries = document.querySelectorAll('.searchBtnRecent');
+
+recentQueries.forEach(recentSearch => {
+    //Adds search function to recentSearch Buttons
+    recentSearch.addEventListener('click', function () {
+        getGeoLoc(this.innerHTML);
+    });
+});
+
+// Event listener to Perform Query for input text
 document.getElementById("searchBtnInput").addEventListener("click", () => {
     event.preventDefault();
     getGeoLoc(document.getElementsByClassName("searchInput")[0].value);
     // getWeather
 });
 
-function getWeather(lat, lon) {
+
+function getForecast(lat, lon) {
     console.log(lat, lon); 
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
 
 };
 
+function getWeather(lat, lon) {
+    console.log(lat, lon); 
+    fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        buildWeather(data);
+    })
+
+};
+
+//Gets Lat & Lon from zipcode or city name and gets current weather and forecast for those coords
 function getGeoLoc(city) {
 
     if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(city)) {
-        fetch("http://api.openweathermap.org/data/2.5/forecast?zip=" + city + ",US&appid=" + apiKey)
+        fetch("http://api.openweathermap.org/geo/1.0/zip?zip=" + city + ",US&appid=" + apiKey)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                getWeather(data.city.coord.lat, data.city.coord.lon);
+                getWeather(data.lat, data.lon);
+                getForecast(data.lat, data.lon);
             })
     } else {
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",US&limit=1&appid=" + apiKey)
